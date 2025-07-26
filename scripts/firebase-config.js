ff349c2f2ed52b7ec9d090dd1e98298f901a28c2
kawser-6T9 <sufiabagum212@gmail.com>
2025-07-26 20:13:20 +0600

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,1 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAhqfXhv2ikNKYDjs5WfEk0R99Az1XPjM8",
+  authDomain: "omrchecker-884d2.firebaseapp.com",
+  projectId: "omrchecker-884d2",
+  storageBucket: "omrchecker-884d2.appspot.com",
+  messagingSenderId: "706020565677",
+  appId: "1:706020565677:web:ef04e86db3caca05841c24"
+};
 
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
